336a6d92dab9c9b20b72d9d4e5d90754036599f3
Your Name <gregoryehill@gmail.com>
2013-05-16 11:38:34 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ddfarm::Application.config.secret_token = '9ac6c211f147be20159796e5027509d4ec99ac45dd11d708e92379a99c988fb4ff5d327c5670dace21fe2b959f2dfc8cd1e016a853f506c7a402eea82fefdc72'
